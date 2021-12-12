d713cf8695c41935256b7efd298a4dd4e14b6e77
Tolstosheyev Roman <izobretatel777@mail.ru>
2021-12-12 13:34:02 +0400

    Add logout

--- src/main/java/com/izobretatel777/ambiglyphserver/controller/JwtAuthenticationController.java
+++ src/main/java/com/izobretatel777/ambiglyphserver/controller/JwtAuthenticationController.java
@@ -8,11 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authenticate")
@@ -28,5 +27,11 @@ public class JwtAuthenticationController {
         UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(jwtRequest.getUsername());
         String token = jwtUtil.generateToken(userDetails);
         return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        return "Bye";
     }
 }
