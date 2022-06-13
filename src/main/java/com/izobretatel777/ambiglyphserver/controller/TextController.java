1a0a36c7d14051e6d6e57fa845bab4e2f180ced3
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-06-13 23:33:46 +0400

    Quick fix of endpoint security

--- src/main/java/com/izobretatel777/ambiglyphserver/controller/TextController.java
+++ src/main/java/com/izobretatel777/ambiglyphserver/controller/TextController.java
@@ -4,8 +4,10 @@ import com.izobretatel777.ambiglyphserver.dto.TextRequestDto;
 import com.izobretatel777.ambiglyphserver.dto.TextResponseDto;
 import com.izobretatel777.ambiglyphserver.service.TextService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TextController {
     private final TextService textService;
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN', 'APP')")
     @Operation(
             summary = "Send text for checking",
             description = """
                     All words that are detected to be spoofed will be replaced with
                     <%%ambiglyph-detected>num<ambiglyph-detected%%>, where num - the number of a detection.
-                    """
+                    Only for authorized users/clients.
+                    """,
+            security = @SecurityRequirement(name = "bearerAuth")
     )
     TextResponseDto checkText(@RequestBody TextRequestDto textRequestDto) {
         return textService.recoverText(textRequestDto);
