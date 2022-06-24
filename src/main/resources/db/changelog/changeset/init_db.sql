1f4c84969f5c1399e4f4ff3269fb3992c60c217a
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-06-24 17:53:02 +0400

    Fix registration

--- src/main/resources/db/changelog/changeset/init_db.sql
+++ src/main/resources/db/changelog/changeset/init_db.sql
@@ -7,7 +7,7 @@ create table if not exists user
     id       int          NOT NULL AUTO_INCREMENT,
     login    varchar(250) NOT NULL UNIQUE,
     password varchar(250) NOT NULL,
-    role     varchar(30)  NOT NULL default 'USER',
+    role     varchar(30)  default 'USER',
     active   boolean      NOT NULL default true,
     PRIMARY KEY (id)
 );
